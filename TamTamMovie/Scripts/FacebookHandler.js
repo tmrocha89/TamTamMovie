165d93b5628cb77d1671d0401c9dfcc62607ac17
Tiago Cruz [1100151] <1100151@isep.ipp.pt>
2015-11-28 18:23:50 +0000

    Facebook share

---
+++ TamTamMovie/Scripts/FacebookHandler.js
@@ -0,0 +1,66 @@
+﻿// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+        shareVideo();
+    } else if (response.status === 'not_authorized') {
+        document.getElementById('status').innerHTML = 'Please log ' +
+          'into this app.';
+    } else {
+        document.getElementById('status').innerHTML = 'Please log ' +
+          'into Facebook.';
+    }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1644308279183059',
+        cookie: true,  // enable cookies to allow the server to access
+        // the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v2.2' // use version 2.2
+    });
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function shareVideo() {
+    var body = 'Reading JS SDK documentation';
+
+    FB.api('/me/feed', 'post', { message: body }, function (response) {
+        if (!response || response.error) {
+            alert("Error post");
+            console.log(response.error);
+        } else {
+            alert('Post Successuful');
+        }
+    });
+}
+
+var login = function (data) {
+    FB.login(function (response) {
+        alert(response);
+    }, { scope: 'email,publish_actions' });
+}
