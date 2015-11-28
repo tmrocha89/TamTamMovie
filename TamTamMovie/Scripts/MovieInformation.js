0de073e5173fbbd0968fcba884e3a4dd00840eb2
Tiago Cruz [1100151] <1100151@isep.ipp.pt>
2015-11-28 18:27:31 +0000

    uncomment update functions

--- TamTamMovie/Scripts/MovieInformation.js
+++ TamTamMovie/Scripts/MovieInformation.js
@@ -1,14 +1,9 @@
 ﻿$(document).ready(function () {
-    /*
     var url = "/Home/GetMoviesBasicInformation";
     $.get(url, null, function (data) {
         data = data + "<script>GetMoviesCover();</script>";
         $('#MovieInformationContent').html(data);        
     });
-    */
-    var AppID = "1644308279183059";
-    var AppSecret = "f4f0d8646292bebd4e7bdeb474ea04a7";
-    var url_autgh = "https://www.facebook.com/dialog/oauth?client_id=" + AppID + "&redirect_uri=http://localhost:51191/Home/Index";
 
 
 });
