9dbf040bf57bb37748324baada5594e8449f37e7
Tiago Cruz [1100151] <1100151@isep.ipp.pt>
2015-11-30 21:54:46 +0000

    Repository for clientWEb added

--- TamTamMovie/Controllers/HomeController.cs
+++ TamTamMovie/Controllers/HomeController.cs
@@ -1,149 +1,46 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Diagnostics;
 using System.Net.Http;
 using TamTamMovie.Models.ModelView;
 using Newtonsoft.Json;
 using System;
+using TamTamMovie.Models.ClientWeb;
 
 namespace TamTamMovie.Controllers
 {
     public class HomeController : AsyncController
     {
 
-        private const string BASE_URL = "http://tamtammovie.azurewebsites.net/";
+        private Repository repository = new Repository(); //from clientWeb
 
         public ActionResult Index(string id)
         {
             ViewBag.Title = "Home Page";
-            IList<MovieDTO> movies = new List<MovieDTO>();
             return View();
         }
 
 
         public async Task<ActionResult> GetMoviesBasicInformation(string movieName)
         {
-            if (!String.IsNullOrWhiteSpace(movieName))
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    client.BaseAddress = new System.Uri(BASE_URL);
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                    string req = "api/Movie/" + movieName + "/false";
-                    HttpResponseMessage response = await client.GetAsync(req);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var strDtoMovies = await response.Content.ReadAsAsync<string>();
-                        IList<MovieDTO> dtoMovies = JsonConvert.DeserializeObject<List<MovieDTO>>(strDtoMovies);
-                        return View("ViewMovieInformation", dtoMovies);
-                    }
-                }
-            }
-            return View("ViewMovieInformation", null);
+            return View("ViewMovieInformation", repository.GetMoviesBasicInformation(movieName));
         }
 
         public async Task<string> LoadCoverFor(string movieID)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = new System.Uri(BASE_URL);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                string req = "api/Movie/"+movieID+"/true?cover=true";
-                HttpResponseMessage response = await client.GetAsync(req);
-                if (response.IsSuccessStatusCode)
-                {
-                    var strDtoMovieImage = await response.Content.ReadAsAsync<string>();
-                    MovieDTO movieDtoImage = JsonConvert.DeserializeObject<MovieDTO>(strDtoMovieImage);
-                    return movieDtoImage.CoverUrl;
-
-                }
-                return "";
-            }
+            return await repository.GetCoverFor(movieID);
         }
 
         public async Task<MovieDTO> LoadDetailedData(string movieID)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = new System.Uri(BASE_URL);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                string req = "api/Movie/" + movieID + "/true?details=true";
-                HttpResponseMessage response = await client.GetAsync(req);
-                if (response.IsSuccessStatusCode)
-                {
-                    var strDtoMovie = await response.Content.ReadAsAsync<string>();
-                    MovieDTO movieDto = JsonConvert.DeserializeObject<MovieDTO>(strDtoMovie);
-                    return movieDto;
-
-                }
-                return null;
-            }
-            //return await repository.LoadDetailedData(movieID);
+            return await repository.LoadDetailedData(movieID);
         }
 
         //used in Javascript
         public async Task<ActionResult> GetMovie(string movieID)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = new System.Uri(BASE_URL);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                string req = "api/Movie/" + movieID + "/true?details=true";
-                HttpResponseMessage response = await client.GetAsync(req);
-                if (response.IsSuccessStatusCode)
-                {
-                    var strDtoMovie = await response.Content.ReadAsAsync<string>();
-                    MovieDTO movieDto = JsonConvert.DeserializeObject<MovieDTO>(strDtoMovie);
-                    return View("ViewMovieDetailInfo", movieDto);
-
-                }
-                return View("ViewMovieDetailInfo", null);
+                return View("ViewMovieDetailInfo", repository.GetMovie(movieID));
             }
-            /*
-            Movie movie = MovieCache.getMovie(movieID);
-            YoutubeVideoProvider yt = new YoutubeVideoProvider();
-            movie.AddTrailers(yt.GetVideoFor(movie.Title.Name));
-
-            return View("ViewMovieDetailInfo", movie);
-            */
         }
-
-        /*
-                public async Task<ActionResult> GetMoviesBasicInformation()
-                {
-                    IList<Movie> movies = await repository.GetMovies("Minions");
-                    return View("ViewMovieInformation", movies);
-                }
-
-                public async Task<string> LoadCoverFor(string movieID)
-                {
-                    Image image = await repository.GetCoverFor(movieID);
-                    if (image != null)
-                    {
-                        return image.Link;
-                    }
-                    return null;
-                }
-
-                public async Task<Movie> LoadDetailedData(string movieID)
-                {
-                    return await repository.LoadDetailedData(movieID);
-                }
-
-                //used in Javascript
-                public async Task<ActionResult> GetMovie(string movieID)
-                {
-                    Movie movie = MovieCache.getMovie(movieID);
-                    YoutubeVideoProvider yt = new YoutubeVideoProvider();
-                    movie.AddTrailers(yt.GetVideoFor(movie.Title.Name));
-
-                    return View("ViewMovieDetailInfo", movie);
-                }
-         */
     }
 }
---
+++ TamTamMovie/Models/ClientWeb/Repository.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TamTamMovie.Models.ModelView;
+
+namespace TamTamMovie.Models.ClientWeb
+{
+    public class Repository
+    {
+        private const string BASE_URL = "http://tamtammovie.azurewebsites.net/"; //this should be dynamically
+
+
+        private IList<MovieDTO> jsonToDtoMovies(string jsonString)
+        {
+            IList<MovieDTO> dtoMovies = JsonConvert.DeserializeObject<List<MovieDTO>>(jsonString);
+            return dtoMovies;
+        }
+
+        private MovieDTO jsonToDtoMovie(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<MovieDTO>(jsonString);
+        }
+
+        //uses application/json
+        private async Task<HttpResponseMessage> getHttpResponseMessage(string urlRequest)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                return await client.GetAsync(urlRequest);
+            }
+        }
+
+        public async Task<IList<MovieDTO>> GetMoviesBasicInformation(string movieName)
+        {
+            if (!String.IsNullOrWhiteSpace(movieName))
+            {
+                string req = "api/Movie/" + movieName + "/false";
+                HttpResponseMessage response = await getHttpResponseMessage(req);
+                if (response.IsSuccessStatusCode)
+                {
+                    var strDtoMovies = await response.Content.ReadAsAsync<string>();
+                    return jsonToDtoMovies(strDtoMovies);
+                }
+            }
+            return (new List<MovieDTO>());
+        }
+
+        public async Task<string> GetCoverFor(string movieID)
+        {
+            string req = "api/Movie/" + movieID + "/true?cover=true";
+            HttpResponseMessage response = await getHttpResponseMessage(req);
+            if (response.IsSuccessStatusCode)
+            {
+                var strDtoMovieImage = await response.Content.ReadAsAsync<string>();
+                MovieDTO movieDtoImage = jsonToDtoMovie(strDtoMovieImage);
+                return movieDtoImage.CoverUrl;
+            }
+            return "";
+
+        }
+
+        public async Task<MovieDTO> LoadDetailedData(string movieID)
+        {
+            string req = "api/Movie/" + movieID + "/true?details=true";
+            HttpResponseMessage response = await getHttpResponseMessage(req);
+            if (response.IsSuccessStatusCode)
+            {
+                var strDtoMovie = await response.Content.ReadAsAsync<string>();
+                MovieDTO movieDto = jsonToDtoMovie(strDtoMovie);
+                return movieDto;
+
+            }
+            return null;
+        }
+
+        //used in Javascript
+        public async Task<MovieDTO> GetMovie(string movieID)
+        {
+            string req = "api/Movie/" + movieID + "/true?details=true";
+            HttpResponseMessage response = await getHttpResponseMessage(req);
+            if (response.IsSuccessStatusCode)
+            {
+                var strDtoMovie = await response.Content.ReadAsAsync<string>();
+                MovieDTO movieDto = JsonConvert.DeserializeObject<MovieDTO>(strDtoMovie);
+                return movieDto;
+            }
+            return null;
+
+        }
+    }
+}
